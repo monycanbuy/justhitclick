36c2b2d75b1e21948503d414edd681356dd2da29
monycanbuy <monycanbuy@yahoo.com>
2024-10-05 12:33:38 +0100

    Update project

--- src/Components/BannerSection/HeroBanner.jsx
+++ src/Components/BannerSection/HeroBanner.jsx
@@ -8,29 +8,17 @@ const HeroBanner = () => {
     <section className="herobg container">
       <div className="hero-text">
         <h1>
-          Bringing
-          <span> Ideas to Life</span> Through Web, Mobile, and IoT Solutions
+          <span> Justhitclick-Digital</span> Technologies Limited
         </h1>
         <p>
-          We're specialized in crafting innovative digital solutions, from
-          custom websites and mobile applications to cutting-edge IoT systems.
-          With a focus on seamless user experiences and robust functionality, I
-          transform your ideas into technology that empowers your business to
-          thrive in the digital age.
+          is a Software development company that routinely offers a web API, an
+          application programming interface for web browsers, or a web interface
+          that is fashioned for a customer’s needs.
         </p>
-        <ul>
-          <li>
-            <Link
-              to="portfolio"
-              smooth="true"
-              offset={-260}
-              duration={500}
-              className="cta-button"
-            >
-              View Portfolio <img src={arrow} alt="" />
-            </Link>
-          </li>
-        </ul>
+        <p>
+          As a company, we drive success through technological innovation,
+          probity, and collaboration.
+        </p>
       </div>
     </section>
   );
